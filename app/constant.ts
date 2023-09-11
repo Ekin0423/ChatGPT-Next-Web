0c9e95c8265011b209d88c8b3bcf4f4120cb4aac
SamMM168 <43960960+SamMM168@users.noreply.github.com>
2023-09-12 04:38:16 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -69,19 +69,11 @@ export const DEFAULT_MODELS = [
     available: true,
   },
   {
-    name: "gpt-3.5-turbo-0301",
-    available: true,
-  },
-  {
     name: "gpt-3.5-turbo-0613",
     available: true,
   },
   {
     name: "gpt-3.5-turbo-16k",
-    available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-16k-0613",
     available: true,
   },
 
