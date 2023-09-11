ec37978a0401b0e60ffeb7ace29af4074ed0bf2d
SamMM168 <43960960+SamMM168@users.noreply.github.com>
2023-09-12 03:25:02 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NewBi ChatGPT
+          Free ChatGPT-3.5
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          QQ：122574263
+          <a href="https://nb.aitom.cc" target="_blank">低价gpt-4</a>
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
